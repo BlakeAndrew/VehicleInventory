f1824158e8a1d975233bb0ac4035ba8a87607407
BlakeAndrew <57735685+BlakeAndrew@users.noreply.github.com>
2019-11-24 00:52:13 -0600

    Added the requisite files via Upload

---
+++ VehicleInventory/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace VehicleInventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vehicleList = LoadVehicles();
+
+            while (true)
+            {
+                Console.WriteLine("Search for the type of Vehicle you are looking for using 'List' for the full list or 'List -car' or type Exit to close the program: ");
+                var userInput = Console.ReadLine();
+
+                if (userInput.Contains("-"))
+                {
+                    var splitter = userInput.Split("-");
+                    var typeValue = splitter[1];
+                    var vehiclesByType = GetVehicleType(typeValue, vehicleList);
+                    ShowVehicles(vehiclesByType);
+                }
+                else if (userInput.ToUpper() == "EXIT")
+                {
+                    break;
+                }
+                else
+                {
+                    ShowVehicles(vehicleList);
+                }
+            }
+
+        }
+
+        private static List<Vehicle> LoadVehicles()
+        {
+            var vehicleList = new List<Vehicle>();
+
+            using (var reader = new StreamReader(@"Files\Vehicles.csv"))
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+
+                    vehicleList.Add(new Vehicle(line));
+                }
+
+            }
+
+            return vehicleList;
+        }
+
+        private static List<Vehicle> GetVehicleType(string vehicleType, List<Vehicle> vehicles)
+        {
+            return vehicles.Where(s => s.VehicleType.Equals(vehicleType, StringComparison.OrdinalIgnoreCase)).ToList();
+        }
+
+        private static void ShowVehicles(List<Vehicle> vehicles)
+        {
+            Console.WriteLine($"Color\tMake\tModel\tYear\tFuel\tType\tVIN" +
+                            $"\n******************************************************************");
+            foreach (var item in vehicles)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ VehicleInventory/Vehicle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace VehicleInventory
+{
+    public class Vehicle
+    {
+        public string Color { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int ModelYear { get; set; }
+        public string FuelType { get; set; }
+        public string VehicleType { get; set; }
+        public string VIN { get; set; }
+
+        public Vehicle(string color, string make, string model, int modelYear, string fuelType, string vehicleType, string vIN)
+        {
+            this.Color = color;
+            this.Make = make;
+            this.Model = model;
+            this.ModelYear = modelYear;
+            this.FuelType = fuelType;
+            this.VehicleType = vehicleType;
+            this.VIN = vIN;
+        }
+
+        public Vehicle(string line)
+        {
+            var columns = line.Split(',');
+
+            this.Color = columns[0];
+            this.Make = columns[1];
+            this.Model = columns[2];
+            this.ModelYear = int.Parse(columns[3]);
+            this.FuelType = columns[4];
+            this.VehicleType = columns[5];
+            this.VIN = columns[6];
+        }
+
+        public override string ToString()
+        {
+            return $"{Color}\t{Make}\t{Model}\t{ModelYear}\t{FuelType}\t{VehicleType}\t{VIN}";
+        }
+
+    }
+}
