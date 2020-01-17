2b4673d6523281edd40942c7c1ab753f50d22b1f
BlakeAndrew <57735685+BlakeAndrew@users.noreply.github.com>
2020-01-16 18:16:26 -0600

    Program specific files

---
+++ VehicleInventory/Models/SUV.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VehicleInventory.Models
+{
+    public class SUV : Vehicle
+    {
+        public int SuvID { get; set; }
+        public int Weight { get; set; }
+
+        public SUV() : base()
+        {
+            Weight = GetWeight();
+            HorsePower = GetHorsePower();
+            Acceleration = GetAcceleration();
+        }
+
+        public SUV(int weight, int horsePower)
+        {
+            this.Weight = weight;
+            this.HorsePower = horsePower;
+        }
+
+        public static int GetWeight()
+        {
+            return Random.Next(2000, 4000);
+        }
+
+        public override double GetAcceleration()
+        {
+            return ((HorsePower * HpToFootPoundPerSecond) / Weight);
+        }
+
+        public int GetHorsePower()
+        {
+            return Random.Next(150, 450);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"\t{Weight}\t\t\t{Acceleration}ft/s²";
+        }
+    }
+}
---
+++ VehicleInventory/Models/Truck.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VehicleInventory.Models
+{
+    public class Truck : Vehicle
+    {
+
+        public int TruckID { get; set; }
+        public int TowingCapacity { get; set; }
+        public int TowWeight { get; set; }
+
+        public Truck() : base()
+        {
+            TowingCapacity = GetTowingCapacity();
+            TowWeight = GetTowWeight();
+            HorsePower = GetHorsePower();
+            Acceleration = GetAcceleration();
+        }
+
+        
+
+        public Truck(int towingCapacity, int towWeight, int horsePower) : base()
+        {
+            this.TowingCapacity = towingCapacity;
+            this.TowWeight = towWeight;
+            this.HorsePower = horsePower;
+        }
+
+        public static int GetTowWeight()
+        {
+            return Random.Next(4000, 12000);
+        }
+
+        public static int GetTowingCapacity()
+        {
+            return Random.Next(500, 10000);
+        }
+
+        public int GetHorsePower()
+        {
+            return Random.Next(200, 600);
+        }
+
+        public override double GetAcceleration()
+        {
+            return ((HorsePower * HpToFootPoundPerSecond) / TowWeight);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"\t{TowWeight}\t{TowingCapacity}\t\t{Acceleration}ft/s²";
+        }
+    }
+}
